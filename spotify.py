3e35f6ae78b7c7e19b6e469ebfe5df902512c48c
Rreact01 <90491061+Rreact01@users.noreply.github.com>
2021-12-08 14:25:47 +0330

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -14,7 +14,7 @@ spotify = spotipy.Spotify(
                                                         client_secret='389614e1ec874f17b8c99511c7baa2f6'))
 genius = lyricsgenius.Genius('biZZReO7F98mji5oz3cE0FiIG73Hh07qoXSIzYSGNN3GBsnY-eUrPAVSdJk_0_de')
 
-token = '5008674339:AAGEWlU0Qn5ZKFeu4P1SoEkOSEpUG7YTq4E'
+token = 'bot token'
 bot = telepot.Bot(token)
 
 
